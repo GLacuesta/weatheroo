0c2a34d5cf9f84d3c7f677757c977626b20e463c
GLacuesta <GLacuesta@users.noreply.github.com>
2022-12-07 18:09:19 +0000

    deploy: 56d7b2ab850b2a7a8af720073b9cf4a330d84032

--- assets/NotFound.3db19b20.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index.3c9248fd.js";const o=()=>t("div",{className:"flex justify-center mt-10",children:"Page not found!"});export{o as default};
---
+++ assets/NotFound.64716579.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index.397fa0a1.js";const o=()=>t("div",{className:"flex justify-center mt-10",children:"Page not found!"});export{o as default};
